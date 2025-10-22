11490147b95aa281d5f5ef308e37b106c4b4dee3
Josiah Nyakwara <josiejosiah89@gmail.com>
2025-10-22 20:10:30 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import "dotenv/config";
 import cors from 'cors';
-import connectDB from './configs/db.js';
+import connectDB from './configs/database.js';
 import adminRouter from './routes/adminRoutes.js';
 import blogRouter from './routes/blogRoutes.js';
 
